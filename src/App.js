ce8ceeacce0ad1179aa57442a3b9d44f8883c316
Leandro Campero <lcampero@onebittech.com>
2022-01-30 22:07:20 -0300

    configuracion de vscode, prettier y modificaciones de codigo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>El fin de todo - React App Demo</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Aplicación desplegada con fleek.co a través de IPFS</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.fleek.co/hosting/site-deployment/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Desplegar una aplicación web con fleek
+        </a>
+        <br />
+        <a
+          className="App-link"
+          href="https://docs.ipfs.io/concepts/what-is-ipfs/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ¿Qué es IPFS?
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
